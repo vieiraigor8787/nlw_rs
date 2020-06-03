0a0d174d5c685807826b08dfaeace9622492d514
vieiraigor8787 <igor_vc@hotmail.com>
2020-06-03 19:26:46 -0300

    cadastro pontos de coleta

--- nlw/server/src/database/migrations/00_create_points.ts
+++ nlw/server/src/database/migrations/00_create_points.ts
@@ -9,7 +9,7 @@ export async function up(knex: Knex) {
         table.string('email').notNullable();
         table.string('whatsapp').notNullable();
         table.decimal('latitude').notNullable();
-        table.decimal('altitude').notNullable();
+        table.decimal('longitude').notNullable();
         table.string('city').notNullable();
         table.string('uf', 2).notNullable();
     })
--- nlw/server/src/routes.ts
+++ nlw/server/src/routes.ts
@@ -8,6 +8,7 @@ routes.get('/items', async (request, response) => {
 
     const serializedItems = items.map(item => {
         return { 
+            id: item.id,
             name: item.title,
             icon_url: `http://localhost:3333/uploads/${item.icon}`
         }
@@ -15,5 +16,44 @@ routes.get('/items', async (request, response) => {
 
     return response.json(serializedItems);
 });
+//cadastro pontos de coleta
+routes.post('/points', async (request, response) => {
+    const { 
+        name,
+        email,
+        whatsapp,
+        latitude,
+        altitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    // transaction() não insere os dados se uma das queries falhar
+    const trx = await knex.transaction()
+
+    const insertedIds = await trx('points').insert({
+        icon: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        altitude,
+        city,
+        uf
+    })
+
+    const point_id = insertedIds[0]
+
+    const pointItems = items.map((item_id: number) => {
+        return { 
+            item_id,
+            point_id
+        }
+    })
+    await trx('point_items').insert(pointItems);
+
+    return response.json({ success: true })
+})
 
 export default routes;
